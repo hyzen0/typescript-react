6cc66be464adbb9316c0601d1616dfc4f2a9c4c3
Rajat Panwar <hyzen@pop-os.localdomain>
2021-11-30 02:37:57 +0530

    AddToCart

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -28,6 +28,16 @@ const App = () => {
     getProducts
   );
   console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddtoCart = (clickedITem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
   return <div className="App">Start</div>;
 };
 
